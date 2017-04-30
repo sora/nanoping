daf34ead24e233c1bb298c44e6ec148068c47e8e
sora <sora@haeena.net>
2017-05-01 00:36:25 +0900

    refactoring

--- nanoping.c
+++ nanoping.c
@@ -27,9 +27,11 @@
 #define likely(x)      __builtin_expect(!!(x), 1)
 #define unlikely(x)    __builtin_expect(!!(x), 0)
 
-#ifndef MSG_ERRQUEUE    // for debug
-#define MSG_ERRQUEUE    0x2000
-#endif
+#define pr_info(S, ...)   printf("\x1b[1m\x1b[94minfo:\x1b[0m " S "\n", ##__VA_ARGS__)
+#define pr_err(S, ...)    fprintf(stderr, "\x1b[1m\x1b[31merror:\x1b[0m " S "\n", ##__VA_ARGS__)
+#define pr_warn(S, ...)   if(warn) fprintf(stderr, "\x1b[1m\x1b[33mwarn :\x1b[0m " S "\n", ##__VA_ARGS__)
+#define pr_debug(S, ...)  if (debug) fprintf(stderr, "\x1b[1m\x1b[90mdebug:\x1b[0m " S "\n", ##__VA_ARGS__)
+
 
 struct hw_timestamp {
 	struct timespec ts;
@@ -43,6 +45,8 @@ static void __attribute__((noreturn)) usage(const char *name)
 	printf("\n%s\n", name);
 	printf("\t -i interface\n");
 	printf("\t -d destination IPv4 address\n");
+	printf("\t -C ping mode\n");
+	printf("\t -D pong mode\n");
 	printf("\t -H Hardware timestamp mode: <tx or rx or both>\n");
 	printf("\n");
 
@@ -61,25 +65,21 @@ static void timeout(int signum)
 	done = true;
 }
 
-static bool tx(int sock, struct sockaddr *addr, socklen_t addr_len)
+static ssize_t tx(int sock, struct sockaddr *addr, socklen_t addr_len)
 {
-	int res;
+	ssize_t count;
 
-	res = sendto(sock, "HELLO", 5, 0, addr, addr_len);
-	if (res < 0)
-		fprintf(stderr, "%s: %s\n", "sendto", strerror(errno));
+	count = sendto(sock, "HELLO", 5, 0, addr, addr_len);
+	if (count < 1)
+		pr_err("%s: %s", "sendto", strerror(errno));
 	else
-		printf("sendto: success\n");
+		pr_info("sendto: success");
 
-	return true;
+	return count;
 }
 
 
-int sk_tx_timeout = 1000;
-static short sk_events = POLLPRI;
-static short sk_revents = POLLPRI;
-
-static int _recvpkt(int fd, struct hw_timestamp *hwts, int recvmsg_flags)
+static ssize_t _recvpkt(int fd, struct hw_timestamp *hwts, int recvmsg_flags)
 {
 	struct timespec *ts = NULL;
 	struct sockaddr_in sin;
@@ -88,8 +88,8 @@ static int _recvpkt(int fd, struct hw_timestamp *hwts, int recvmsg_flags)
 	char ctrl[512] = {};
 	char buf[256] = {};
 	int level, type;
-	int count = 0;
-	int res = 0;
+	ssize_t count;
+	int res;
 
 	struct cmsghdr *cm;
 
@@ -104,22 +104,22 @@ static int _recvpkt(int fd, struct hw_timestamp *hwts, int recvmsg_flags)
 	iov.iov_len = sizeof(buf);
 
 	if (recvmsg_flags == MSG_ERRQUEUE) {
-		struct pollfd pfd = { fd, sk_events, 0 };
-		res = poll(&pfd, 1, sk_tx_timeout);
+		struct pollfd pfd = { fd, POLLPRI, 0 };
+		res = poll(&pfd, 1, 1);
 		if (res < 1) {
-			fprintf(stderr, "%s: res=%d: %s\n", "poll: tx timestamp", res, strerror(errno));
+			pr_err("%s: res=%d: %s", "poll: tx timestamp", res, strerror(errno));
 			return res;
-		} else if (!(pfd.revents & sk_revents)) {
-			fprintf(stderr, "%s: %s\n", "poll: non ERR event", strerror(errno));
+		} else if (!(pfd.revents & POLLPRI)) {
+			pr_err("%s: %s", "poll: non ERR event", strerror(errno));
 			return -1;
 		}
 	}
 
 	count = recvmsg(fd, &msg, recvmsg_flags);
 	if (count < 1) {
-		fprintf(stderr, "%s: %s\n", "recvmsg", strerror(errno));
+		pr_err("%s: %s", "recvmsg", strerror(errno));
 	} else {
-		printf("recvmsg: success\n");
+		pr_info("recvmsg: success");
 	}
 
 	for (cm = CMSG_FIRSTHDR(&msg); cm != NULL; cm = CMSG_NXTHDR(&msg, cm)) {
@@ -135,19 +135,23 @@ static int _recvpkt(int fd, struct hw_timestamp *hwts, int recvmsg_flags)
 		}
 	}
 
-	printf("ts0: %lld.%.9ld\n", (long long)ts[0].tv_sec, ts[0].tv_nsec);
-	printf("ts1: %lld.%.9ld\n", (long long)ts[1].tv_sec, ts[1].tv_nsec);
-	printf("ts2: %lld.%.9ld\n", (long long)ts[2].tv_sec, ts[2].tv_nsec);
+	pr_info("ts0: %lld.%.9ld", (long long)ts[0].tv_sec, ts[0].tv_nsec);
+	pr_info("ts1: %lld.%.9ld", (long long)ts[1].tv_sec, ts[1].tv_nsec);
+	pr_info("ts2: %lld.%.9ld", (long long)ts[2].tv_sec, ts[2].tv_nsec);
+	pr_info("ts3: %lld.%.9ld", (long long)ts[3].tv_sec, ts[3].tv_nsec);
+	pr_info("ts4: %lld.%.9ld", (long long)ts[4].tv_sec, ts[4].tv_nsec);
+	pr_info("ts5: %lld.%.9ld", (long long)ts[5].tv_sec, ts[5].tv_nsec);
+	pr_info("ts6: %lld.%.9ld", (long long)ts[6].tv_sec, ts[6].tv_nsec);
 
 	return count;
 }
 
-static inline bool rx(int sock)
+static inline ssize_t rx(int sock)
 {
 	return _recvpkt(sock, NULL, 0);
 }
 
-static inline bool read_timestamp(int fd, struct hw_timestamp *hwts)
+static inline ssize_t read_timestamp(int fd, struct hw_timestamp *hwts)
 {
 
 	return _recvpkt(fd, hwts, MSG_ERRQUEUE);
@@ -156,19 +160,22 @@ static inline bool read_timestamp(int fd, struct hw_timestamp *hwts)
 int main(int argc, char **argv)
 {
 	// getopt
+	enum _ping_mode { NO_MODE, PING_MODE, PONG_MODE } ping_mode = NO_MODE;
 	enum _rx_mode { NONE, ALL } rx_mode = NONE;
 	enum _tx_mode { OFF, ON } tx_mode = OFF;
 	const char *ifname = 0;
 	const char *dst = 0;
 
-	unsigned int txsockopt = SOF_TIMESTAMPING_TX_HARDWARE | SOF_TIMESTAMPING_RAW_HARDWARE;
+	unsigned int txskopt = SOF_TIMESTAMPING_TX_HARDWARE |
+	                       SOF_TIMESTAMPING_RAW_HARDWARE;
 	struct sockaddr_in addr_tx = {0}, addr_rx = {0};
 	int sock_tx, sock_rx;
 	struct ifreq dev;
+	ssize_t count;
 	int res;
 
 	int ch;
-	while ((ch = getopt(argc, argv, "i:d:H:")) != -1) {
+	while ((ch = getopt(argc, argv, "CDi:d:H:")) != -1) {
 		switch (ch) {
 			case 'i':
 				ifname = optarg;
@@ -187,38 +194,46 @@ int main(int argc, char **argv)
 					rx_mode = ALL;
 					tx_mode = ON;
 				} else {
-					printf("none\n");
-					fprintf(stderr, "%s: %s\n", "-H", "Unknown mode");
+					pr_err("%s: %s", "-H", "Unknown mode");
 					exit(1);
 				}
+				break;
+			case 'C':
+				if (ping_mode == PONG_MODE)
+					usage(argv[0]);
+				ping_mode = PING_MODE;
+				break;
+			case 'D':
+				if (ping_mode == PING_MODE)
+					usage(argv[0]);
+				ping_mode = PONG_MODE;
 				break;
 			default:
 				usage(argv[0]);
 		}
 	}
-	if (ifname == 0 || dst == 0) {
+	if (ifname == 0 || dst == 0 || ping_mode == NO_MODE) {
 		usage(argv[0]);
 	}
 
 	// hw_config
-	printf("txmode: %d, rxmode: %d\n", tx_mode, rx_mode);
 	res = hwtstamp_config_set(ifname, tx_mode, rx_mode);
 	if (res < 0) {
-		fprintf(stderr, "%s: %s\n", "hwtstamp_config_set", strerror(errno));
+		pr_err("%s: %s", "hwtstamp_config_set", strerror(errno));
 		return 1;
 	}
 
 	// tx	
 	sock_tx = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sock_tx < 0) {
-		fprintf(stderr, "%s: %s\n", "socket", strerror(errno));
+		pr_err("%s: %s", "socket", strerror(errno));
 		exit(1);
 	}
 
 	memset(&dev, 0, sizeof(dev));
 	strncpy(dev.ifr_name, ifname, sizeof(dev.ifr_name));
 	if (ioctl(sock_tx, SIOCGIFADDR, &dev) < 0) {
-		fprintf(stderr, "%s: %s\n", "ioctl: SIOCGIFADDR", strerror(errno));
+		pr_err("%s: %s", "ioctl: SIOCGIFADDR", strerror(errno));
 		exit(1);
 	}
 
@@ -226,16 +241,16 @@ int main(int argc, char **argv)
 	addr_tx.sin_port = htons(12345);
 	addr_tx.sin_addr.s_addr = inet_addr(dst);
 
-	if (setsockopt(sock_tx, SOL_SOCKET, SO_TIMESTAMPING, (char *)&txsockopt, sizeof(txsockopt)))
+	if (setsockopt(sock_tx, SOL_SOCKET, SO_TIMESTAMPING, (char *)&txskopt, sizeof(txskopt)))
 		error(1, 0, "setsockopt timestamping");
-	if (setsockopt(sock_tx, SOL_SOCKET, SO_SELECT_ERR_QUEUE, (char *)&txsockopt, sizeof(txsockopt)))
+	if (setsockopt(sock_tx, SOL_SOCKET, SO_SELECT_ERR_QUEUE, (char *)&txskopt, sizeof(txskopt)))
 		error(1, 0, "setsockopt timestamping");
 
 
 	// rx
 	sock_rx = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sock_rx < 0) {
-		fprintf(stderr, "%s: %s\n", "socket", strerror(errno));
+		pr_err("%s: %s", "socket", strerror(errno));
 		exit(1);
 	}
 
@@ -254,9 +269,9 @@ int main(int argc, char **argv)
 		uint64_t diff;
 		
 		// send the packet
-		res = tx(sock_tx, (struct sockaddr *)&addr_tx, sizeof(addr_tx));
-		if (res) {
-			res = read_timestamp(sock_tx, &hwts0);
+		count = tx(sock_tx, (struct sockaddr *)&addr_tx, sizeof(addr_tx));
+		if (count > 0) {
+			count = read_timestamp(sock_tx, &hwts0);
 		}
 
 		// get the current time
@@ -292,3 +307,4 @@ int main(int argc, char **argv)
 	close(sock_tx);
 	close(sock_rx);
 }
+
